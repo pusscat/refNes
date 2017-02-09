000953091ede6123bab412533ad52efd2e7ff0f8
Onca <pusscat@gmail.com>
2017-02-09 15:21:00 -0500

    Make a minor change and some notes about instructions modifying PC

--- instructions.py
+++ instructions.py
@@ -6,10 +6,14 @@ class Instruction(object):
         self.size = size
 
     def execute(cpu):
-        self.function(cpu)
-        cpu.incPC(self.size)
+        # emulate the instruction and add instruction size to PC
+        # if the function returns false implying it hasnt changed PC
+        if self.function(cpu) is False:
+            cpu.incPC(self.size)
         cpu.incCycles(self.cycles)
 
+# instruction functions return True if they modify PC
+# return false otherwise
 
 def adcImm(cpu):
     pass
