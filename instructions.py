43f7ac7b17ea40b076e21a0e17e58c7431befa49
Onca <pusscat@gmail.com>
2017-02-11 14:11:33 -0500

    Refactored to nothingness :)
    I think this is as compact as this can be now

--- instructions.py
+++ instructions.py
@@ -1,7 +1,8 @@
 class Instruction(object):
-    def __init__(self, mnem, function, flags, size, cycles):
+    def __init__(self, mnem, function, operType, flags, size, cycles):
         self.mnem = mnem
         self.function = function
+        self.operType = operType
         self.flags = flags
         self.cycles = cycles
         self.size = size
@@ -51,7 +52,8 @@ def GetAddress(cpu, operType):
     return address
 
 
-def doAdc(cpu, instruction, value):
+def doAdc(cpu, instruction):
+    value = GetValue(cpu, self.operType)
     accuVal = cpu.GetRegister('A')
     carryVal = 1 if cpu.GetFlag('C') else 0
 
@@ -61,40 +63,8 @@ def doAdc(cpu, instruction, value):
     cpu.UpdateFlags(instruction.flags, accuVal, immVal, newVal, False, False)
     return False
 
-
-def adcImm(cpu, instruction):
-    immVal = GetValue(cpu, 'IMM')
-    return doAdc(cpu, instruction, immVal)
-
-def adcZero(cpu, instruction):
-    memVal= GetValue(cpu, 'ZERO')
-    return doAdc(cpu, instruction, memVal)
-
-def adcZeroX(cpu, instruction):
-    memVal = GetValue(cpu, 'ZEROX')
-    return doAdc(cpu, instruction, memVal)
-
-def adcAbs(cpu, instruction):
-    memVal = GetValue(cpu, 'ABS')
-    return doAdc(cpu, instruction, memVal)
-
-def adcAbsX(cpu, instruction):
-    memVal = GetValue(cpu, 'ABSX')
-    return doAdc(cpu, instruction, memVal)
-
-def adcAbsY(cpu, instruction):
-    memVal = GetValue(cpu, 'ABSY')
-    return doAdc(cpu, instruction, memVal)
-
-def adcIndX(cpu, instruction):
-    memVal = GetValue(cpu, 'INDX')
-    return doAdc(cpu, instruction, memVal)
-
-def adcIndY(cpu, instruction):
-    memVal = GetValue(cpu, 'INDY')
-    return doAdc(cpu, instruction, memVal)
-
-def doAnd(cpu, instruction, value):
+def doAnd(cpu, instruction):
+    value = GetValue(cpu, self.operType)
     accuVal = cpu.GetRegister('A')
 
     newVal = accuVal & immVal
@@ -103,39 +73,8 @@ def doAnd(cpu, instruction, value):
     cpu.UpdateFlags(instruction.flags, accuVal, immVal, newVal, False, False)
     return False
 
-def andImm(cpu, instruction):
-    immVal = GetValue(cpu, 'IMM')
-    return doAnd(cpu, instruction, immVal)
-
-def andZero(cpu, instruction):
-    memVal = GetValue(cpu, 'ZERO')
-    return doAnd(cpu, instruction, memVal)
-
-def andZeroX(cpu, instruction):
-    memVal = GetValue(cpu, 'ZEROX')
-    return doAnd(cpu, instruction, memVal)
-
-def andAbs(cpu, instruction):
-    memVal = GetValue(cpu, 'ABS')
-    return doAnd(cpu, instruction, memVal)
-
-def andAbsX(cpu, instruction):
-    memVal = GetValue(cpu, 'ABSX')
-    return doAnd(cpu, instruction, memVal)
-
-def andAbsY(cpu, instruction):
-    memVal = GetValue(cpu, 'ABSY')
-    return doAnd(cpu, instruction, memVal)
-
-def andIndX(cpu, instruction):
-    memVal = GetValue(cpu, 'INDX')
-    return doAnd(cpu, instruction, memVal)
-
-def andIndY(cpu, instruction):
-    memVal = GetValue(cpu, 'INDY')
-    return doAnd(cpu, instruction, memVal)
-
-def doAsl(cpu, instruction, address):
+def doAsl(cpu, instruction):
+    addrVal = GetAddress(cpu, self.operType)
     memVal = cpu.ReadMemory(address)
 
     newVal = memVal << 1
@@ -146,7 +85,7 @@ def doAsl(cpu, instruction, address):
     cpu.UpdateFlags(intruction.flags, memVal, memVal, newVal, False, False)
     return False
 
-def aslAccu(cpu, instruction):
+def doAslAccu(cpu, instruction):
     accuVal = cpu.GetRegister('A')
 
     newVal = accuVal << 1
@@ -158,25 +97,6 @@ def aslAccu(cpu, instruction):
     cpu.UpdateFlags(intruction.flags, accuVal, accuVal, newVal, False, False)
     return False
 
-def aslZero(cpu, instruction):
-    addrVal = GetAddress(cpu, 'ZERO')
-    return doAsl(cpu, instruction, addrVal)
-
-def aslZeroX(cpu, instruction):
-    addrVal = GetAddress(cpu, 'ZEROX')
-    return doAsl(cpu, instruction, addrVal)
-
-def aslAbs(cpu, instruction):
-    addrVal = GetAddress(cpu, 'ABS')
-    return doAsl(cpu, instruction, addrVal)
-
-def aslAbsX(cpu, instruction):
-    addrVal = GetAddress(cpu, 'ABSX')
-    return doAsl(cpu, instruction, addrVal)
-
-
-
-
 # http://www.e-tradition.net/bytes/6502/6502_instruction_set.html - Appendix A
 flags = {   'ADC', ['N', 'Z', 'C', 'V'],
             'AND', ['N', 'Z'],
@@ -184,25 +104,25 @@ flags = {   'ADC', ['N', 'Z', 'C', 'V'],
         }
 
            # opcode : Instruction(mnem, function, size, cycles), 
-instructions = {0x69: Instruction('ADCimm', adcImm, flags['ADC'], 2, 2),
-                0x65: Instruction('ADCzero', adcZero, flags['ADC'], 2, 3),
-                0x75: Instruction('ADCzeroX', adcZeroX, flags['ADC'], 2, 4),
-                0x6D: Instruction('ADCabs', adcAbs, flags['ADC'], 3, 4),
-                0x7D: Instruction('ADCabsX', adcAbsX, flags['ADC'], 3, 4),
-                0x79: Instruction('ADCabsY', adcAbsY, flags['ADC'], 3, 4),
-                0x61: Instruction('ADCindX', adcIndX, flags['ADC'], 2, 6),
-                0x71: Instruction('ADCindY', adcIndY, flags['ADC'], 2, 5),
-                0x29: Instruction('ANDimm', andImm, flags['AND'], 2, 2),
-                0x25: Instruction('ANDzero', andImm, flags['AND'], 2, 3),
-                0x35: Instruction('ANDzeroX', andImm, flags['AND'], 2, 4),
-                0x2d: Instruction('ANDabs', andImm, flags['AND'], 3, 4),
-                0x3d: Instruction('ANDabsX', andImm, flags['AND'], 3, 4),
-                0x39: Instruction('ANDabsY', andImm, flags['AND'], 3, 4),
-                0x21: Instruction('ANDindX', andImm, flags['AND'], 2, 6),
-                0x31: Instruction('ANDindY', andImm, flags['AND'], 2, 5),
-                0x0A: Instruction('ASLaccu', aslAccu, flags['ASL'], 1, 2),
-                0x06: Instruction('ASLzero', aslZero, flags['ASL'], 2, 5),
-                0x16: Instruction('ASLzeroX', aslZeroX, flags['ASL'], 2, 6),
-                0x0E: Instruction('ASLabs', aslAbs, flags['ASL'], 3, 6),
-                0x1E: Instruction('ASLabsX', aslAbsX, flags['ASL'], 3, 7),
+instructions = {0x69: Instruction('ADC', doAdc, 'IMM', flags['ADC'], 2, 2),
+                0x65: Instruction('ADC', doAdc, 'ZERO' flags['ADC'], 2, 3),
+                0x75: Instruction('ADC', doAdc, 'ZEROX', flags['ADC'], 2, 4),
+                0x6D: Instruction('ADC', doAdc, 'ABS', flags['ADC'], 3, 4),
+                0x7D: Instruction('ADC', doAdc, 'ABSX', flags['ADC'], 3, 4),
+                0x79: Instruction('ADC', doAdc, 'ABSY', flags['ADC'], 3, 4),
+                0x61: Instruction('ADC', doAdc, 'INDX', flags['ADC'], 2, 6),
+                0x71: Instruction('ADC', doAdc, 'INDY', flags['ADC'], 2, 5),
+                0x29: Instruction('AND', doAnd, 'IMM', flags['AND'], 2, 2),
+                0x25: Instruction('AND', doAnd, 'ZERO', flags['AND'], 2, 3),
+                0x35: Instruction('AND', doAnd, 'ZEROX', flags['AND'], 2, 4),
+                0x2d: Instruction('AND', doAnd, 'ABS', flags['AND'], 3, 4),
+                0x3d: Instruction('AND', doAnd, 'ABSX', flags['AND'], 3, 4),
+                0x39: Instruction('AND', doAnd, 'ABSY', flags['AND'], 3, 4),
+                0x21: Instruction('AND', doAnd, 'INDX', flags['AND'], 2, 6),
+                0x31: Instruction('AND', doAnd, 'INDY', flags['AND'], 2, 5),
+                0x0A: Instruction('ASL', doAslAccu, '', flags['ASL'], 1, 2),
+                0x06: Instruction('ASL', doAsl, 'ZERO', flags['ASL'], 2, 5),
+                0x16: Instruction('ASL', doAsl, 'ZEROX', flags['ASL'], 2, 6),
+                0x0E: Instruction('ASL', doAsl, 'ABS', flags['ASL'], 3, 6),
+                0x1E: Instruction('ASL', doAsl, 'ABSX', flags['ASL'], 3, 7),
                 }
