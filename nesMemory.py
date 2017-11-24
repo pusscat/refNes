dd8cbe93c045dfb9ce07183416b7bf530f8dbe00
pusscat <pusscat@gmail.com>
2017-11-24 16:47:51 -0500

    Do some basic mirroring
    Allow for different mem blocks for later mappers

--- nesMemory.py
+++ nesMemory.py
@@ -6,16 +6,29 @@ class Memory(object):
         self.memory = 0x10000 * [0]
 
     def AddressTranslation(self, address):
-        # handle mirroring here
-        newAddr = address
-        return newAddr
+        # handle basic mirroring here
+        if address > 0x7FF and address < 0x2000:
+            address &= 0x7FF
+
+        # handle ppu i/o register mirroring here
+        if address > 0x2007 and address < 0x4000:
+            address = 0x2000 + (address % 8)
+       
+        # handle nametable mirroring here - XXX
+
+        # handle all other mapper specific mirroring - XXX
+        
+        # pass back both memory block and address so mappers 
+        # dont have to copy to base memory, and can pass their 
+        # own blocks
+        return (self.memory, address)
 
 
     def ReadMemory(self, address):
-        addr = self.AddressTranslation(address)
-        return self.memory[addr]
+        (mem, addr) = self.AddressTranslation(address)
+        return mem[addr]
 
     def SetMemory(self, address, value):
-        addr = self.AddressTranslation(address)
-        self.memory[addr] = value & 0xFF
+        (mem, addr) = self.AddressTranslation(address)
+        mem[addr] = value & 0xFF
         return value & 0xFF
